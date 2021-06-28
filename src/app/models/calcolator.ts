8052583f303f0e8f728762544caeadb8193037a0
ilaria-source <ilaria.secchi@avanade.com>
2021-06-28 16:54:27 +0200

    appare la calcolatrice classica, brutta ma c'è. Continuo le modifiche per renderla leggibile

---
+++ src/app/models/calcolator.ts
@@ -0,0 +1,10 @@
+import { TypeCalc } from './enums/typeCalculator.model';
+//si crea un file ts chiamato nomeClasse.model.ts
+
+export class CalcolatorType {
+  constructor(
+    public typeName: TypeCalc,
+    public url: string,
+    public isOn: boolean
+    ){}
+}
