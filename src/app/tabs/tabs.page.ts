73b7ef363e53ba06d833753f3ce4b11e39ebb8b4
ilaria-source <ilaria.secchi@avanade.com>
2021-07-06 11:21:11 +0200

    pianificazione calcolatrice scientifica e studio comandi

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,5 +1,3 @@
-import { isNgTemplate } from '@angular/compiler';
-import { dashCaseToCamelCase } from '@angular/compiler/src/util';
 import { Component } from '@angular/core';
 import { TypeCalc } from '../models/enums/typeCalculator.model';
 
@@ -12,14 +10,8 @@ export class TabsPage {
   public dataCalcList: {typeName: TypeCalc; icon: string; isOn: boolean; element: string}[] =
   [
     {typeName: TypeCalc.classic,icon: 'bug',  isOn: true, element: 'tab2'},
-    {typeName: TypeCalc.scientific,icon: 'construct',  isOn: false, element: 'tab3'},
+    {typeName: TypeCalc.scientific,icon: 'construct',  isOn: true, element: 'tab3'},
     {typeName: TypeCalc.programmer,icon: 'save',  isOn: false, element: 'tab4'}
   ];
 
-  // dataCalcList.forEach(function(item){
-  //   if (item.isOn == false)
-  //   {
-  //     item.element = '' ;
-  //   }
-  // }
 }
